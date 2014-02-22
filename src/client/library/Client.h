075e5e277b3f0f1c633cb89d6b69518eb87fa257
Erick Barrera <ebarr019@fiu.edu>
2014-02-21 22:48:45 -0500

    Update Client.h
    Added function prototypes and removed unused variables and functions.
    I don't think we need all those includes but I'll take out the unused ones eventually.

--- src/client/library/Client.h
+++ src/client/library/Client.h
@@ -1,22 +1,27 @@
+#pragma once
+#pragma comment(lib, "Ws2_32.lib")
 //#define _WIN32_WINNT 0x501
-
+#include "stdafx.h"
 #include <winsock2.h>
 #include <windows.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#ifndef CLIENT_H_INCLUDED
-#define CLIENT_H_INCLUDED
+//#ifndef CLIENT_H_INCLUDED
+//#define CLIENT_H_INCLUDED
 
 class Client
 {
-public:
-    int iResult;
-    SOCKET connectSocket;
-    UploadLog();
-    Client(void);
-    ~Client(void);
+	public:
+		Client(void);
+		~Client(void);
+		SOCKET connectSocket;
+		int iResult;  
 };
+void Listen(SOCKET csock);
+void UploadLog(SOCKET csock);
+void SendTestMesg(SOCKET csock);
 
 
 #endif // CLIENT_H_INCLUDED
